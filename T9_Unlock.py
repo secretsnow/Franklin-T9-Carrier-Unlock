8d0b28f6ea02ffb6a9c84f7d927a59f698990e91
secretsnow <44920124+secretsnow@users.noreply.github.com>
2023-12-16 11:12:40 -0600

    Update T9_Unlock.py

--- T9_Unlock.py
+++ T9_Unlock.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-# This is the shebang line that tells the operating system which interpreter to use
-from __future__ import print_function # This enables the print function in Python 2
-import hashlib # This imports the hashlib module for computing hashes
-import sys # This imports the sys module for accessing standard input and output
 
-# Your code goes here
-print("Enter your IMEI: ", end="") # This prints a prompt without a newline
-IMEI = sys.stdin.readline().strip() # This reads a line from standard input and removes the trailing newline
-hash = hashlib.sha1(IMEI + "simlock").hexdigest()[:8] # This computes the SHA1 hash of the IMEI concatenated with "simlock" and takes the first 8 characters
-print(hash, end="") # This prints the hash without a newline
+from __future__ import print_function # enables the print function in Python 2
+import hashlib # imports the hashlib module for computing hashes
+import sys # imports the sys module for accessing standard input and output
+
+print("Enter your IMEI: ", end="") # prints a prompt without a newline
+IMEI = sys.stdin.readline().strip() # reads a line from standard input and removes the trailing newline
+hash = hashlib.sha1(IMEI + "simlock").hexdigest()[:8] # computes SHA1 hash of the IMEI concatenated with "simlock" and takes the first 8 characters
+print(hash, end="") # prints the hash without a newline
