d2d3ec137d94ef35c50990fff46ce03d613d1971
secretsnow <44920124+secretsnow@users.noreply.github.com>
2023-12-16 09:50:40 -0600

    Delete T9_Unlock.py

--- T9_Unlock.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys # provides access to system functions
-import hashlib # provides hash algorithms
-print("Enter your IMEI: ", end="") #  displays a prompt without a newline
-IMEI = sys.stdin.readline().strip() # reads a line from standard input and removes the trailing newline
-hash = hashlib.sha1((IMEI + "simlock").encode()).hexdigest() # computes SHA1 hash of input, then converts it to hexadecimal
-print(hash[:8]) # prints first 8 chara of the hash
